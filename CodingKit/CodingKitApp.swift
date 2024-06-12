2f7f61b7a2b8c0641971f136c25aac8941b9fea2
Arman Nihal <armansaliknihal@gmail.com>
2024-06-12 21:41:19 +0600

    Initial Commit

---
+++ CodingKit/CodingKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingKitApp.swift
+//  CodingKit
+//
+//  Created by Arman Nihal on 12.6.2024.
+//
+
+import SwiftUI
+
+@main
+struct CodingKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
