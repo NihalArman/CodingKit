895dacc9179025d910f6a701e98f33e9e236f8c2
Arman Nihal <armansaliknihal@gmail.com>
2024-06-19 14:20:37 +0600

    Initialize fields in login view

--- CodingKit/LoginView.swift
+++ CodingKit/LoginView.swift
@@ -11,30 +11,39 @@ import SwiftUI
 struct LoginView: View {
     @State private var userName: String = ""
     @State private var password: String = ""
+    @State private var isLoggedIn: Bool = false
 
     var body: some View {
         NavigationView {
             VStack {
-                 TextField(
-                     "User Name",
-                     text: $userName
-                 )
-                 .disableAutocorrection(true)
-                 SecureField(
-                     "Password",
-                     text: $password
-                 )
-                 .disableAutocorrection(true)
+                TextField(
+                    "User Name",
+                    text: $userName
+                )
+                .disableAutocorrection(true)
+                SecureField(
+                    "Password",
+                    text: $password
+                )
+                .disableAutocorrection(true)
 
                 HStack {
-                    Button("Sign In", action: {})
-                    Button("Register", action: {})
+                    Button("Sign In", action: userSignIn)
+
+                    Button("Register", action: userRegister)
                 }
                 .buttonStyle(.bordered)
-             }
+            }
             .textFieldStyle(.roundedBorder)
             .navigationTitle("Sign in or Register")
         }
+    }
+    
+    private func userSignIn() {
+        isLoggedIn.toggle()
+    }
+
+    private func userRegister() {
     }
 }
 
