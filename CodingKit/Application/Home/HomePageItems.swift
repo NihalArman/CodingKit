f4674b04d4df5fe405569af137eff3eb6da1ba78
Arman Nihal <armansaliknihal@gmail.com>
2024-06-19 19:29:57 +0600

    Initialize Home Page items

---
+++ CodingKit/Application/Home/HomePageItems.swift
@@ -0,0 +1,24 @@
+//
+//  HomePageItems.swift
+//  CodingKit
+//
+//  Created by Arman Nihal on 19.6.2024.
+//
+
+import Foundation
+
+struct HomePageItemModel {
+    let id: Int
+    let name: String
+    let iconName: String
+}
+
+struct HomePageItems {
+    static let memo = HomePageItemModel(id: 001, name: "Memo", iconName: "")
+    static let tddChecklist = HomePageItemModel(id: 002, name: "TDD Checklist", iconName: "")
+    static let notes = HomePageItemModel(id: 003, name: "Notes", iconName: "")
+    static let rotationTimer = HomePageItemModel(id: 004, name: "Rotation Timer", iconName: "")
+    static let sessionTimer = HomePageItemModel(id: 005, name: "Session Timer", iconName: "")
+
+    static let items = [memo, tddChecklist, notes, rotationTimer, sessionTimer]
+}
