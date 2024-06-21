ed18f6b778e9b08aaab3dce249dd7f7b0c0f94ea
Arman Nihal <armansaliknihal@gmail.com>
2024-06-21 15:36:38 +0600

    Create emptly session timer view page

---
+++ CodingKit/Source/Home/Applications/SessionTimer/SessionTimerView.swift
@@ -0,0 +1,22 @@
+//
+//  SessionTimerView.swift
+//  CodingKit
+//
+//  Created by Arman Nihal on 21.6.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct SessionTimerView: View {
+    var body: some View {
+        NavigationView {
+            Text("Session Timer Page")
+                .navigationTitle("Session Timer")
+        }
+    }
+}
+
+#Preview {
+    SessionTimerView()
+}
