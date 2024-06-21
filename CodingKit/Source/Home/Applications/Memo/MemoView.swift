373b1d9bdefbd75c4f86c06f78f391470522a414
Arman Nihal <armansaliknihal@gmail.com>
2024-06-21 15:43:48 +0600

    Create emptly memo view page

---
+++ CodingKit/Source/Home/Applications/Memo/MemoView.swift
@@ -0,0 +1,22 @@
+//
+//  MemoView.swift
+//  CodingKit
+//
+//  Created by Arman Nihal on 21.6.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct MemoView: View {
+    var body: some View {
+        NavigationView {
+            Text("Memo Page")
+                .navigationTitle("Memo")
+        }
+    }
+}
+
+#Preview {
+    MemoView()
+}
