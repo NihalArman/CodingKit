06c5b18c22282ae8955855d3fc02202eb3236aff
Arman Nihal <armansaliknihal@gmail.com>
2024-06-21 15:40:13 +0600

    Create emptly notes view page

---
+++ CodingKit/Source/Home/Applications/Notes/NotesView.swift
@@ -0,0 +1,23 @@
+//
+//  NotesView.swift
+//  CodingKit
+//
+//  Created by Arman Nihal on 21.6.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        NavigationView {
+            Text("Notes Page")
+                .navigationTitle("Notes")
+        }
+    }
+}
+
+#Preview {
+    NotesView()
+}
+
