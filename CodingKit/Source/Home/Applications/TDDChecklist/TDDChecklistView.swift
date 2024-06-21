f95a1489f887c78a99876809ba7661bdd2f29d29
Arman Nihal <armansaliknihal@gmail.com>
2024-06-21 15:42:12 +0600

    Create emptly TDD Checklist view page

---
+++ CodingKit/Source/Home/Applications/TDDChecklist/TDDChecklistView.swift
@@ -0,0 +1,22 @@
+//
+//  TDDChecklistView.swift
+//  CodingKit
+//
+//  Created by Arman Nihal on 21.6.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct TDDChecklistView: View {
+    var body: some View {
+        NavigationView {
+            Text("TDD Checklist Page")
+                .navigationTitle("TDD Checklist")
+        }
+    }
+}
+
+#Preview {
+    TDDChecklistView()
+}
