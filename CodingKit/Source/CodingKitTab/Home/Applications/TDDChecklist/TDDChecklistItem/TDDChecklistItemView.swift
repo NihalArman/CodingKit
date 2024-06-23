9fb8cb9360666fb23938506e96c597e8249584a0
Arman Nihal <armansaliknihal@gmail.com>
2024-06-23 13:56:32 +0600

    Initialize empty individual TDD checklist item view and view model

---
+++ CodingKit/Source/CodingKitTab/Home/Applications/TDDChecklist/TDDChecklistItem/TDDChecklistItemView.swift
@@ -0,0 +1,24 @@
+//
+//  TDDChecklistItemView.swift
+//  CodingKit
+//
+//  Created by Arman Nihal on 23.6.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct TDDChecklistItemView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("TDD Checklist Item Page")
+            }
+            .navigationTitle("Title")
+        }
+    }
+}
+
+#Preview {
+    TDDChecklistItemView()
+}
---
+++ CodingKit/Source/CodingKitTab/Home/Applications/TDDChecklist/TDDChecklistItem/TDDChecklistItemViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TDDChecklistItemViewModel.swift
+//  CodingKit
+//
+//  Created by Arman Nihal on 23.6.2024.
+//
+
+import Foundation
+
+final class TDDChecklistItemViewModel: ObservableObject {
+    init() {
+    }
+}
