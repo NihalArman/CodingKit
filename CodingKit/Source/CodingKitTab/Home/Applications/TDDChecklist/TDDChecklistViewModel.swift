c6744715b8f12181da03e7de3c84bf0cf13637f6
Arman Nihal <armansaliknihal@gmail.com>
2024-06-23 13:51:44 +0600

    Initialize empty TDDChecklist view model

---
+++ CodingKit/Source/CodingKitTab/Home/Applications/TDDChecklist/TDDChecklistViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TDDChecklistViewModel.swift
+//  CodingKit
+//
+//  Created by Arman Nihal on 23.6.2024.
+//
+
+import Foundation
+
+final class TDDChecklistViewModel: ObservableObject {
+    init() {
+    }
+}
